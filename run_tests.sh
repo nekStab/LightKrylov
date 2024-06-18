ab937a806b1f3d38de209b5e8656220b5515114b
Jean-Christophe <loiseau.jc@gmail.com>
2024-06-18 10:08:29 +0200

    Add utility script to run the tests.

---
+++ run_tests.sh
@@ -0,0 +1,13 @@
+# Gfortran.
+python deployment.py
+
+fpm test --compiler "gfortran" --flag "-O0 -g3 -fbacktrace -Wall -Wextra -fcheck=all -pedantic -Wconversion -fbounds-check -ffpe-trap=zero,overflow,underflow" --verbose
+#fpm test --compiler "gfortran" --flag "-O3 -march=native -mtune=native"
+
+# Ifort
+#fpm test --compiler "ifort"
+#fpm test --compiler "ifort" --flag "-O3 -xhost"
+
+# Ifx
+#fpm test --compiler "ifx"
+#fpm test --compiler "ifx" --flag "-O3 -xhost"
