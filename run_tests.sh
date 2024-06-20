8fd4aba8f510481d0e6609a0bdb0ad583fc8def8
Jean-Christophe <loiseau.jc@gmail.com>
2024-06-20 09:37:50 +0200

    Updated run_tests with correct compilation options.

--- run_tests.sh
+++ run_tests.sh
@@ -2,12 +2,12 @@
 python deployment.py
 
 fpm test --compiler "gfortran" --flag "-O0 -g3 -fbacktrace -Wall -Wextra -fcheck=all -pedantic -Wconversion -fbounds-check -ffpe-trap=zero,overflow" --verbose
-fpm test --compiler "gfortran" --flag "-O3 -march=native -mtune=native"
+fpm test --compiler "gfortran" --profile release --flag "-march=native -mtune=native"
 
 # Ifort
-#fpm test --compiler "ifort"
-#fpm test --compiler "ifort" --flag "-O3 -xhost"
+fpm test --compiler "ifort" --flag "-O0 -g -traceback -check all -check bounds -check all -check uninit, -ftrapuv -debug all" --verbose
+fpm test --compiler "ifort" --profile release --flag "-xHost"
 
 # Ifx
 #fpm test --compiler "ifx"
-#fpm test --compiler "ifx" --flag "-O3 -xhost"
+#fpm test --compiler "ifx" --profile release --flag "-O3 -xHost"
