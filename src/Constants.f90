68f3838980f5e25d42e146d3a260915b0ceb6d72
Simon Kern <skern@mech.kth.se>
2024-07-17 13:41:31 +0200

    added mpi functions

--- src/Constants.f90
+++ src/Constants.f90
@@ -1,14 +1,11 @@
-module lightkrylov_constants
-#ifdef MPI
-    use mpi
-#endif
+module LightKrylov_Constants
+    use mpi_f08
     implicit none
     private
-#ifdef MPI
-    integer , parameter, public :: nid = myrank
-#else
-    integer , parameter, public :: nid = 0
-#endif
+
+    integer, private :: nio = 1
+    integer, private :: nid = 1
+    integer, private :: comm_size = 1
 
     integer , parameter, public :: sp = selected_real_kind(6, 37)
     !! Definition of the single precision data type.
@@ -35,4 +32,50 @@ module lightkrylov_constants
     complex(sp), parameter, public :: one_im_cdp = cmplx(0.0_dp, 1.0_dp, kind=dp)
     complex(dp), parameter, public :: zero_cdp   = cmplx(0.0_dp, 0.0_dp, kind=dp)
 
-end module lightkrylov_constants
+    ! MPI subroutines
+    public :: mpi_initialize, mpi_close
+    ! Getter/setter
+    public :: set_io_rank
+    public :: io_rank
+    public :: get_rank
+    
+contains
+
+   subroutine mpi_initialize()
+      integer :: ierr
+      ! Initialize MPI
+      call MPI_Init(ierr)
+      if (ierr /= MPI_SUCCESS) then
+         print *, "Error initializing MPI"
+         STOP 1
+      end if
+      call MPI_Comm_rank(MPI_COMM_WORLD, nid, ierr)
+      call MPI_Comm_size(MPI_COMM_WORLD, comm_size, ierr)
+   end subroutine mpi_initialize
+
+   subroutine mpi_close
+      integer :: ierr
+      ! Finalize MPI
+      call MPI_Finalize(ierr)
+      if (ierr /= MPI_SUCCESS) then
+          print *, "Error finalizing MPI"
+          STOP 1
+      end if
+   end subroutine mpi_close
+
+   subroutine set_io_rank(rk)
+      integer, intent(in) :: rk
+      if (rk > comm_size) print *, 'Invalid I/O rank specified in set_io_rank'
+      nio = rk
+   end 
+
+   logical function io_rank() result(is_io)
+      is_io = .false.      
+      if (nid == nio) is_io = .true.
+   end function io_rank
+
+   integer function get_rank() result(rank)
+      rank = nid
+   end function get_rank
+
+end module LightKrylov_Constants
