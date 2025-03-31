7d350cb36b13544d4976e1bf3a36f927582715d9
Jean-Christophe <loiseau.jc@gmail.com>
2025-03-31 18:04:20 +0200

    Last test

--- test/TestVectors.f90
+++ test/TestVectors.f90
@@ -147,16 +147,23 @@ contains
         x = dense_vector(x_) ; call x%rand(ifnorm=.true.)
         y = x
         call random_number(alpha)
-        print *, "alpha : ", alpha, "\n"
-        print *, "x     : ", x%data, "\n"
-        print *, "y     : ", x%data, "\n"
-        print *, "x-y     : ", x%data - y%data, "\n"
+        print *, "alpha : ", alpha
+        print *
+        print *, "x     : ", x%data
+        print *
+        print *, "y     : ", x%data
+        print *
+        print *, "x-y     : ", x%data - y%data
         
         ! Scale the vector.
         call x%scal(alpha)
-        print *, "scal(x) :", x%data, "\n"
-        print *, "scal(y) :", alpha*y%data, "\n"
-        print *, "diff    :", abs(x%data - alpha*y%data), "\n"
+        print *
+        print *, "scal(x) :", x%data
+        print *
+        print *, "scal(y) :", alpha*y%data
+        print *
+        print *, "diff    :", abs(x%data - alpha*y%data)
+        print *
         print *, "norm(diff) :", norm(x%data - alpha*y%data, 2) 
 
         ! Check correctness.
@@ -282,16 +289,23 @@ contains
         x = dense_vector(x_) ; call x%rand(ifnorm=.true.)
         y = x
         call random_number(alpha)
-        print *, "alpha : ", alpha, "\n"
-        print *, "x     : ", x%data, "\n"
-        print *, "y     : ", x%data, "\n"
-        print *, "x-y     : ", x%data - y%data, "\n"
+        print *, "alpha : ", alpha
+        print *
+        print *, "x     : ", x%data
+        print *
+        print *, "y     : ", x%data
+        print *
+        print *, "x-y     : ", x%data - y%data
         
         ! Scale the vector.
         call x%scal(alpha)
-        print *, "scal(x) :", x%data, "\n"
-        print *, "scal(y) :", alpha*y%data, "\n"
-        print *, "diff    :", abs(x%data - alpha*y%data), "\n"
+        print *
+        print *, "scal(x) :", x%data
+        print *
+        print *, "scal(y) :", alpha*y%data
+        print *
+        print *, "diff    :", abs(x%data - alpha*y%data)
+        print *
         print *, "norm(diff) :", norm(x%data - alpha*y%data, 2) 
 
         ! Check correctness.
@@ -418,16 +432,23 @@ contains
         y = x
         alpha = 0.0_sp ; call random_number(alpha%re) ; call random_number(alpha%im)
         alpha = alpha / abs(alpha)
-        print *, "alpha : ", alpha, "\n"
-        print *, "x     : ", x%data, "\n"
-        print *, "y     : ", x%data, "\n"
-        print *, "x-y     : ", x%data - y%data, "\n"
+        print *, "alpha : ", alpha
+        print *
+        print *, "x     : ", x%data
+        print *
+        print *, "y     : ", x%data
+        print *
+        print *, "x-y     : ", x%data - y%data
         
         ! Scale the vector.
         call x%scal(alpha)
-        print *, "scal(x) :", x%data, "\n"
-        print *, "scal(y) :", alpha*y%data, "\n"
-        print *, "diff    :", abs(x%data - alpha*y%data), "\n"
+        print *
+        print *, "scal(x) :", x%data
+        print *
+        print *, "scal(y) :", alpha*y%data
+        print *
+        print *, "diff    :", abs(x%data - alpha*y%data)
+        print *
         print *, "norm(diff) :", norm(x%data - alpha*y%data, 2) 
 
         ! Check correctness.
@@ -554,16 +575,23 @@ contains
         y = x
         alpha = 0.0_dp ; call random_number(alpha%re) ; call random_number(alpha%im)
         alpha = alpha / abs(alpha)
-        print *, "alpha : ", alpha, "\n"
-        print *, "x     : ", x%data, "\n"
-        print *, "y     : ", x%data, "\n"
-        print *, "x-y     : ", x%data - y%data, "\n"
+        print *, "alpha : ", alpha
+        print *
+        print *, "x     : ", x%data
+        print *
+        print *, "y     : ", x%data
+        print *
+        print *, "x-y     : ", x%data - y%data
         
         ! Scale the vector.
         call x%scal(alpha)
-        print *, "scal(x) :", x%data, "\n"
-        print *, "scal(y) :", alpha*y%data, "\n"
-        print *, "diff    :", abs(x%data - alpha*y%data), "\n"
+        print *
+        print *, "scal(x) :", x%data
+        print *
+        print *, "scal(y) :", alpha*y%data
+        print *
+        print *, "diff    :", abs(x%data - alpha*y%data)
+        print *
         print *, "norm(diff) :", norm(x%data - alpha*y%data, 2) 
 
         ! Check correctness.
