a210ab622628333432b2ce08253981bee7b18cbb
Simon KERN <johann-simon.kern@ensam.eu>
2025-07-17 16:05:49 +0200

    line break added

--- src/Utilities/Logger.f90
+++ src/Utilities/Logger.f90
@@ -601,7 +601,8 @@ contains
                write(msg,'(A,I0,A)') 'GMRES iteration converged after ', info, ' iterations'
                call log_message(trim(msg), module=module, procedure=procedure)
             else if (info < 0) then
-               write(msg,'(A,I0,A)') 'Maximum number of GMRES iterations reached (', abs(info), '). Solution tolerance not achieved.'
+               write(msg,'(A,I0,A)') 'Maximum number of GMRES iterations reached (', abs(info), &
+                           & '). Solution tolerance not achieved.'
                call log_message(trim(msg), module=module, procedure=procedure)
             else
                write(msg,'(A)') "Undocumented error. "//trim(str)
@@ -614,7 +615,8 @@ contains
                write(msg,'(A,I0,A)') 'CG iteration converged after ', info, ' iterations'
                call log_message(trim(msg), module=module, procedure=procedure)
             else if (info < 0) then
-               write(msg,'(A,I0,A)') 'Maximum number of CG iterations reached (', abs(info), '). Solution tolerance not achieved.'
+               write(msg,'(A,I0,A)') 'Maximum number of CG iterations reached (', abs(info), &
+                           & '). Solution tolerance not achieved.'
                call log_message(trim(msg), module=module, procedure=procedure)
             else
                write(msg,'(A)') "Undocumented error. "//trim(str)
@@ -627,7 +629,8 @@ contains
                write(msg,'(A,I0,A)') 'The linear solver converged after ', info, ' iterations'
                call log_message(trim(msg), module=module, procedure=procedure)
             else if (info < 0) then
-               write(msg,'(A,I0,A)') 'Maximum number of iterations reached (', abs(info), '). Solution tolerance not achieved.'
+               write(msg,'(A,I0,A)') 'Maximum number of iterations reached (', abs(info), &
+                           & '). Solution tolerance not achieved.'
                call log_message(trim(msg), module=module, procedure=procedure)
             else
                write(msg,'(A)') "Undocumented error. "//trim(str)
