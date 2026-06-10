feb4807782213cb5d82de8cb368d515f83506d70
Simon KERN <johann-simon.kern@ensam.eu>
2026-06-11 00:44:27 +0200

    biorthogonalization of 2 bases. First implementation

--- src/Krylov/utilities.f90
+++ src/Krylov/utilities.f90
@@ -245,4 +245,229 @@ contains
         end if
     end procedure is_orthonormal_cdp
 
+
+    module procedure biorthonormalize_bases_rsp
+        character(len=*), parameter :: this_procedure = 'biorthonormalize_basis_rsp'
+        !! SVD workspace
+        integer                     :: n, m, ns, i, info_, nretain
+        real(sp), allocatable       :: M(:,:), U(:,:), VT(:,:)
+        real(sp), allocatable :: S(:)
+        real(sp)              :: tol_
+
+        if (time_lightkrylov()) call timer%start(this_procedure)
+
+        ! handle optional tol
+        tol_ = optval(tol, atol_sp)
+        n = size(X)
+        m = size(Y)
+        ns = min(n, m)
+        allocate(M(n, m), S(ns), U(n, ns), VT(ns, m))
+
+        ! compute SVD of inner product matrix
+        M = innerprod(Y, X)
+        call svd(M, S, U, VT)
+
+        ! determine how many singular values are above relative tolerance
+        nretain = 0
+        do i = 1, ns
+            if (S(i) / S(1) > tol_) then
+                nretain = nretain + 1
+            end if
+        end do
+
+        info_ = 0
+        if (nretain == 0) info_ = -1
+        call check_info(info, 'biorthonormalize_bases', this_module, this_procedure)
+
+        ! renormalize retained singular values
+        do i = 1, nretain
+            S(i) = one_rsp / sqrt(S(i))
+        end do
+        ! zero out truncated part
+        S(nretain+1:) = zero_rsp
+
+        ! apply symmetric transformation in-place
+        block
+            class(abstract_vector_rsp), allocatable :: Xwrk(:)
+            call linear_combination(Xwrk, X, matmul(transpose(VT), diag(S)))
+            call copy(X, Xwrk)
+            call linear_combination(Xwrk, Y, matmul(U, diag(S)))
+            call copy(Y, Xwrk)
+        end block
+
+        info_ = nretain
+
+        if (present(info)) info = merge(nretain, info_, info_ == 0)
+        if (time_lightkrylov()) call timer%stop(this_procedure)
+
+    end procedure biorthonormalize_bases_rsp
+
+    module procedure biorthonormalize_bases_rdp
+        character(len=*), parameter :: this_procedure = 'biorthonormalize_basis_rdp'
+        !! SVD workspace
+        integer                     :: n, m, ns, i, info_, nretain
+        real(dp), allocatable       :: M(:,:), U(:,:), VT(:,:)
+        real(dp), allocatable :: S(:)
+        real(dp)              :: tol_
+
+        if (time_lightkrylov()) call timer%start(this_procedure)
+
+        ! handle optional tol
+        tol_ = optval(tol, atol_dp)
+        n = size(X)
+        m = size(Y)
+        ns = min(n, m)
+        allocate(M(n, m), S(ns), U(n, ns), VT(ns, m))
+
+        ! compute SVD of inner product matrix
+        M = innerprod(Y, X)
+        call svd(M, S, U, VT)
+
+        ! determine how many singular values are above relative tolerance
+        nretain = 0
+        do i = 1, ns
+            if (S(i) / S(1) > tol_) then
+                nretain = nretain + 1
+            end if
+        end do
+
+        info_ = 0
+        if (nretain == 0) info_ = -1
+        call check_info(info, 'biorthonormalize_bases', this_module, this_procedure)
+
+        ! renormalize retained singular values
+        do i = 1, nretain
+            S(i) = one_rdp / sqrt(S(i))
+        end do
+        ! zero out truncated part
+        S(nretain+1:) = zero_rdp
+
+        ! apply symmetric transformation in-place
+        block
+            class(abstract_vector_rdp), allocatable :: Xwrk(:)
+            call linear_combination(Xwrk, X, matmul(transpose(VT), diag(S)))
+            call copy(X, Xwrk)
+            call linear_combination(Xwrk, Y, matmul(U, diag(S)))
+            call copy(Y, Xwrk)
+        end block
+
+        info_ = nretain
+
+        if (present(info)) info = merge(nretain, info_, info_ == 0)
+        if (time_lightkrylov()) call timer%stop(this_procedure)
+
+    end procedure biorthonormalize_bases_rdp
+
+    module procedure biorthonormalize_bases_csp
+        character(len=*), parameter :: this_procedure = 'biorthonormalize_basis_csp'
+        !! SVD workspace
+        integer                     :: n, m, ns, i, info_, nretain
+        complex(sp), allocatable       :: M(:,:), U(:,:), VT(:,:)
+        real(sp), allocatable :: S(:)
+        real(sp)              :: tol_
+
+        if (time_lightkrylov()) call timer%start(this_procedure)
+
+        ! handle optional tol
+        tol_ = optval(tol, atol_sp)
+        n = size(X)
+        m = size(Y)
+        ns = min(n, m)
+        allocate(M(n, m), S(ns), U(n, ns), VT(ns, m))
+
+        ! compute SVD of inner product matrix
+        M = innerprod(Y, X)
+        call svd(M, S, U, VT)
+
+        ! determine how many singular values are above relative tolerance
+        nretain = 0
+        do i = 1, ns
+            if (S(i) / S(1) > tol_) then
+                nretain = nretain + 1
+            end if
+        end do
+
+        info_ = 0
+        if (nretain == 0) info_ = -1
+        call check_info(info, 'biorthonormalize_bases', this_module, this_procedure)
+
+        ! renormalize retained singular values
+        do i = 1, nretain
+            S(i) = one_rsp / sqrt(S(i))
+        end do
+        ! zero out truncated part
+        S(nretain+1:) = zero_rsp
+
+        ! apply symmetric transformation in-place
+        block
+            class(abstract_vector_csp), allocatable :: Xwrk(:)
+            call linear_combination(Xwrk, X, matmul(transpose(VT), diag(S)))
+            call copy(X, Xwrk)
+            call linear_combination(Xwrk, Y, matmul(U, diag(S)))
+            call copy(Y, Xwrk)
+        end block
+
+        info_ = nretain
+
+        if (present(info)) info = merge(nretain, info_, info_ == 0)
+        if (time_lightkrylov()) call timer%stop(this_procedure)
+
+    end procedure biorthonormalize_bases_csp
+
+    module procedure biorthonormalize_bases_cdp
+        character(len=*), parameter :: this_procedure = 'biorthonormalize_basis_cdp'
+        !! SVD workspace
+        integer                     :: n, m, ns, i, info_, nretain
+        complex(dp), allocatable       :: M(:,:), U(:,:), VT(:,:)
+        real(dp), allocatable :: S(:)
+        real(dp)              :: tol_
+
+        if (time_lightkrylov()) call timer%start(this_procedure)
+
+        ! handle optional tol
+        tol_ = optval(tol, atol_dp)
+        n = size(X)
+        m = size(Y)
+        ns = min(n, m)
+        allocate(M(n, m), S(ns), U(n, ns), VT(ns, m))
+
+        ! compute SVD of inner product matrix
+        M = innerprod(Y, X)
+        call svd(M, S, U, VT)
+
+        ! determine how many singular values are above relative tolerance
+        nretain = 0
+        do i = 1, ns
+            if (S(i) / S(1) > tol_) then
+                nretain = nretain + 1
+            end if
+        end do
+
+        info_ = 0
+        if (nretain == 0) info_ = -1
+        call check_info(info, 'biorthonormalize_bases', this_module, this_procedure)
+
+        ! renormalize retained singular values
+        do i = 1, nretain
+            S(i) = one_rdp / sqrt(S(i))
+        end do
+        ! zero out truncated part
+        S(nretain+1:) = zero_rdp
+
+        ! apply symmetric transformation in-place
+        block
+            class(abstract_vector_cdp), allocatable :: Xwrk(:)
+            call linear_combination(Xwrk, X, matmul(transpose(VT), diag(S)))
+            call copy(X, Xwrk)
+            call linear_combination(Xwrk, Y, matmul(U, diag(S)))
+            call copy(Y, Xwrk)
+        end block
+
+        info_ = nretain
+
+        if (present(info)) info = merge(nretain, info_, info_ == 0)
+        if (time_lightkrylov()) call timer%stop(this_procedure)
+
+    end procedure biorthonormalize_bases_cdp
+
 end submodule krylov_utilities
